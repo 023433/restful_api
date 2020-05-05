19f92809dcc54a7d0692c3c04802ab07e9f27bad
jhk7375 <jhk7375@naver.com>
2020-05-05 22:36:24 +0900

    댓글 컨트롤러 수정

--- src/main/java/dev/j/api/restful/blog/controller/ControllerComment.java
+++ src/main/java/dev/j/api/restful/blog/controller/ControllerComment.java
@@ -11,8 +11,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -68,5 +71,181 @@ public class ControllerComment {
     public ResponseEntity<Page<Comment>> getCommentsNewest() {
         return new ResponseEntity<Page<Comment>>(serviceComment.getCommentsNewest(), HttpStatus.OK);
     }
+
+
+
+    @ApiOperation(
+        value = "댓글 등록",
+        response = ResponseEntity.class
+    )
+    @ApiImplicitParams({
+        @ApiImplicitParam(
+            name = "X-Auth-Token", 
+            required = false, 
+            paramType = "header", 
+            dataTypeClass = String.class
+        ) 
+    })
+    @PostMapping("/comment/reply")
+    public ResponseEntity<String> commentReply(
+        @ApiParam(value = "이름", required = false) 
+        @RequestParam(value = "name", required = false)
+        String name, 
+        
+        @ApiParam(value = "비밀번호", required = false) 
+        @RequestParam(value = "password", required = false)
+        String password,
+
+        @ApiParam(value = "댓글 번호", required = true) 
+        @RequestParam(value = "no", required = true)
+        String no,
+        
+        @ApiParam(value = "내용", required = true) 
+        @RequestParam(value = "content", required = true)
+        String content,
+        
+        @ApiParam(value = "비공개 여부", required = true) 
+        @RequestParam(value = "secret", required = true)
+        String secret) {
+
+            
+
+        System.out.println(name);
+        System.out.println(no);
+        System.out.println(password);
+        System.out.println(content);
+        System.out.println(secret);
+        HttpStatus http = HttpStatus.OK;
+
+
+        return new ResponseEntity<>(http);
+        
+    }
+
+
+    @ApiOperation(
+        value = "댓글 수정",
+        response = ResponseEntity.class
+    )
+    @ApiImplicitParams({
+        @ApiImplicitParam(
+            name = "X-Auth-Token", 
+            required = false, 
+            paramType = "header", 
+            dataTypeClass = String.class
+        ) 
+    })
+    @PutMapping("/comment")
+    public ResponseEntity<String> commentModify(
+        @ApiParam(value = "이름", required = false) 
+        @RequestParam(value = "name", required = false)
+        String name, 
+        
+        @ApiParam(value = "비밀번호", required = false) 
+        @RequestParam(value = "password", required = false)
+        String password,
+
+        @ApiParam(value = "댓글 번호", required = true) 
+        @RequestParam(value = "no", required = true)
+        String no,
+        
+        @ApiParam(value = "내용", required = true) 
+        @RequestParam(value = "content", required = true)
+        String content,
+        
+        @ApiParam(value = "비공개 여부", required = true) 
+        @RequestParam(value = "secret", required = true)
+        String secret) {
+
+            
+
+        System.out.println(name);
+        System.out.println(no);
+        System.out.println(password);
+        System.out.println(content);
+        System.out.println(secret);
+        HttpStatus http = HttpStatus.OK;
+
+
+        return new ResponseEntity<>(http);
+        
+    }
+
+
+    @ApiOperation(
+        value = "댓글 등록",
+        response = ResponseEntity.class
+    )
+    @ApiImplicitParams({
+        @ApiImplicitParam(
+            name = "X-Auth-Token", 
+            required = false, 
+            paramType = "header", 
+            dataTypeClass = String.class
+        ) 
+    })
+    @PostMapping("/comment")
+    public ResponseEntity<String> commentInsert(
+        @ApiParam(value = "이름", required = false) 
+        @RequestParam(value = "name", required = false)
+        String name, 
+        
+        @ApiParam(value = "비밀번호", required = false) 
+        @RequestParam(value = "password", required = false)
+        String password,
+
+        @ApiParam(value = "내용", required = true) 
+        @RequestParam(value = "content", required = true)
+        String content,
+        
+        @ApiParam(value = "비공개 여부", required = true) 
+        @RequestParam(value = "secret", required = true)
+        String secret) {
+
+
+        System.out.println(name);
+        System.out.println(password);
+        System.out.println(content);
+        System.out.println(secret);
+        HttpStatus http = HttpStatus.OK;
+
+
+        return new ResponseEntity<>(http);
+        
+    }
+
+
+    @ApiOperation(
+        value = "댓글 등록",
+        response = ResponseEntity.class
+    )
+    @ApiImplicitParams({
+        @ApiImplicitParam(
+            name = "X-Auth-Token", 
+            required = false, 
+            paramType = "header", 
+            dataTypeClass = String.class
+        ) 
+    })
+    @DeleteMapping("/comment")
+    public ResponseEntity<String> commentDelete(
+        @ApiParam(value = "비밀번호", required = false) 
+        @RequestParam(value = "password", required = false)
+        String password,
+
+        @ApiParam(value = "댓글 번호", required = true) 
+        @RequestParam(value = "no", required = true)
+        String no) {
+
+
+        System.out.println(no);
+        System.out.println(password);
+        HttpStatus http = HttpStatus.OK;
+
+
+        return new ResponseEntity<>(http);
+        
+    }
+
 
 }
