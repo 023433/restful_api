230abb68d805e64884eba8740b3e26e4f84571a9
jhk7375 <jhk7375@naver.com>
2020-05-17 17:30:46 +0900

    path

--- src/main/java/dev/j/api/restful/common/property/PropertyPath.java
+++ src/main/java/dev/j/api/restful/common/property/PropertyPath.java
@@ -3,7 +3,7 @@ package dev.j.api.restful.common.property;
 public class PropertyPath {
 
 	public static final String IMG_DEV = "D:\\img";
-	public static final String IMG_PRD = "/usr/share/nginx/html/data/upload/";
+	public static final String IMG_PRD = "/upload/";
 
 	public static final String PROFILE_IMG = "profile/";
 }
