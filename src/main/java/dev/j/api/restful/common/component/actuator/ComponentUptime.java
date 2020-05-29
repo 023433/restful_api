6de2756d0c8284528e624ff0d0a579c158cc8e0f
jhk7375 <jhk7375@naver.com>
2020-05-29 22:26:20 +0900

    uptime

--- src/main/java/dev/j/api/restful/common/component/actuator/ComponentUptime.java
+++ src/main/java/dev/j/api/restful/common/component/actuator/ComponentUptime.java
@@ -27,6 +27,7 @@ public class ComponentUptime extends AbstractComponent {
   private double getLinuxUptime(){
     String[] cmd = {"/bin/sh", "-c", "uptime -s"};
     String uptime = executorToString(cmd);
+    System.out.println(uptime);
     return extracted(uptime);
   }
 
