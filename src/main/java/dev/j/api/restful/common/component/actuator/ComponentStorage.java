5b2cccbfe3d1bb025a6d2f39562b00dbc60bbb41
jhk7375 <jhk7375@naver.com>
2020-05-31 19:10:11 +0900

    dir

--- src/main/java/dev/j/api/restful/common/component/actuator/ComponentStorage.java
+++ src/main/java/dev/j/api/restful/common/component/actuator/ComponentStorage.java
@@ -11,7 +11,7 @@ public class ComponentStorage extends AbstractComponent {
         if (isWindows()) {
 
         } else if (isUnix()) {
-            String[] cmd = { "/bin/sh", "-c", "cd " + dir, "du -s"};
+            String[] cmd = { "/bin/sh", "-c", "cd " + dir, "pwd"};
             size = executorToString(cmd);
         }
 
