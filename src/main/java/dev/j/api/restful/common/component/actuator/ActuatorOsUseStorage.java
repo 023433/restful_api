46600a9e1f1a045c4ca059568a51ba8626aa907b
jhk7375 <jhk7375@naver.com>
2020-05-31 18:49:02 +0900

    dir

--- src/main/java/dev/j/api/restful/common/component/actuator/ActuatorOsUseStorage.java
+++ src/main/java/dev/j/api/restful/common/component/actuator/ActuatorOsUseStorage.java
@@ -1,13 +1,17 @@
 package dev.j.api.restful.common.component.actuator;
 
 
-import javax.servlet.http.HttpServletRequest;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.actuate.endpoint.web.annotation.RestControllerEndpoint;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Component
@@ -17,17 +21,30 @@ public class ActuatorOsUseStorage {
     @Autowired
     private ComponentStorage componentStorage;
 
+    @ApiOperation(
+        value = "사용량 요청",
+        response = ResponseEntity.class
+    )
+    @ApiImplicitParams({
+        @ApiImplicitParam(
+            name = "X-Auth-Token", 
+            required = true, 
+            paramType = "header", 
+            dataTypeClass = String.class
+        ) 
+    })
     @GetMapping("/storage")
-    public @ResponseBody ResponseEntity<String> getServerInfo(HttpServletRequest request) {
-        System.out.println(request.getParameterValues("name"));
-
-        String[] dir = request.getParameterValues("dir");
+    public @ResponseBody ResponseEntity<String> getServerInfo(
+        @ApiParam(value = "폴더명", required = true) 
+        @RequestParam(value = "dir", required = true)
+        String[] dir) {
 
         if(dir == null){
-            return  new ResponseEntity<String>("ss", HttpStatus.BAD_REQUEST);
+            return  new ResponseEntity<String>("err", HttpStatus.BAD_REQUEST);
         }
         
         for (String str : dir) {
+            System.out.println(str);
             System.out.println(componentStorage.getDirSize(str));
         }
 
