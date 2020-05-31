f73373d686ab51a83144db1b21de546d2e2a12b8
jhk7375 <jhk7375@naver.com>
2020-05-31 19:48:02 +0900

    dir

--- src/main/java/dev/j/api/restful/common/component/actuator/ComponentStorage.java
+++ src/main/java/dev/j/api/restful/common/component/actuator/ComponentStorage.java
@@ -1,36 +1,19 @@
 package dev.j.api.restful.common.component.actuator;
 
-import java.io.File;
-import java.io.IOException;
-import java.io.InputStream;
-import java.nio.charset.StandardCharsets;
-import org.apache.commons.io.IOUtils;
 import org.springframework.stereotype.Component;
 
 @Component
 public class ComponentStorage extends AbstractComponent {
 
-    public String getDirSize(String dir) {
+	public String getDirSize(String dir) {
         String size = "";
 
         if (isWindows()) {
 
         } else if (isUnix()) {
-            // create a process and execute
-            try {
-            System.out.println("aaaa : " + dir);
-            String[] cmd = { "bash", "-c", "(du -s)"};
-
-                Process p = Runtime.getRuntime().exec(cmd, null, new File(dir));
-                InputStream inputStream = p.getInputStream();
-                return IOUtils.toString(inputStream, StandardCharsets.UTF_8);
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
             System.out.println("sss : " + dir);
-            String[] cmd = { "/bin/sh", "-c", "(du -s '" + dir + "')"};
-
-            size = executorToString(cmd);
+            String[] cmd = { "/bin/sh", "-c", "(du -s " +")"};
+            size = executorToString("bash -c (cd " + dir + "; du -s )");
         }
 
 		return size;
